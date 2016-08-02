1c5c77450fe64fb52977a5e2dd69e68be93d7934
George <yscherba@speroteck.com>
2016-08-02 16:42:50 +0300

    adding npm start command and some conf

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,14 @@ var webpack = require('webpack');
 module.exports = {
     entry: './js/app.js',
     output: { path: __dirname, filename: 'bundle.js' },
+    devServer: {
+        inline: true,
+        port: 3333
+    },
     module: {
         loaders: [
             {
-                test: /.js?$/,
+                test: /\.js?$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/,
                 query: {
